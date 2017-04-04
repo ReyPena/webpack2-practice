7d9a47f9d2ada635f5fa7a3b780538f6009e697f
ReyPena <zonickblack@gmail.com>
2017-04-04 15:30:16 -0600

    Finish basics

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -1,19 +1,46 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
   module: {
     rules: [
       {
-        use: 'babel-loader',
-        test: /\.js$/
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test:/\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        })
+      },
+      {
+        test:/\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          {
+            loader: 'image-webpack-loader',
+            query: {}
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new ProgressBarPlugin()
+  ]
 };
 
 module.exports = config;
