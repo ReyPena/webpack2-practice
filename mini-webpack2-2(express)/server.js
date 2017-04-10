2c04bb8cc93d4f2bbc6b8937df821a31020cc1d3
ReyPena <zonickblack@gmail.com>
2017-04-10 08:40:21 -0600

    Add production envarioment

--- mini-webpack2-2(express)/server.js
+++ mini-webpack2-2(express)/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
+const path = require('path');
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
 
-console.log(webpack);
-app.listen(3050, () => console.log('Listening in port 3050'));
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  })
+}
+
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
